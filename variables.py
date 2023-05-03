6f5787214ede1902d7aefd703115c64a8a9c49f4
Andrew Viola <andrew@viola.dev>
2023-05-02 23:13:42 -0400

    Removed test.py

--- variables.py
+++ variables.py
@@ -3,13 +3,13 @@ URL = "http://localhost/" # Default: http://localhost, adjust if connection issu
 grabGcode = "{}server/files/list?root=gcodes".format(URL)
 
 # Set your schedule in 24 hour format ie 8:42 PM is "20:42" - Edit these!
-Monday = 
-Tuesday = 
-Wednesday = 
-Thursday = 
-Friday = 
-Saturday = 
-Sunday = 
+Monday = "12:00"
+Tuesday = "12:00"
+Wednesday = "12:00"
+Thursday = "12:00"
+Friday = "12:00"
+Saturday = "12:00"
+Sunday = "12:00"
 
 # Only one of these can be selected true. If multiple were to be selected,
 # iteratedListPrint will take precedence, then memoryPrint and then trueRandom
